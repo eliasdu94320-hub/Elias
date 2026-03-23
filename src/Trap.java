9f4c30692b71159a1173cf43c8412955bec039fb
Elias Abssi <eliasdu94320@gmail.com>
2026-03-23 18:38:32 +0100

    Ajout d'un rocher + personnage qui bouge avec flèches + (je viens encore de modifier et là je viens de mettre toute la map) + (je viens de mettre des pièges qui me mettent des dégâts)

---
+++ src/Trap.java
@@ -0,0 +1,8 @@
+import java.awt.Image;
+
+public class Trap extends SolidSprite {
+
+    public Trap(double x, double y, Image image, double width, double height) {
+        super(x, y, image, width, height);
+    }
+}
